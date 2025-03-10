673a0d4892a144ba24f768bca03a4e85ab472bec
Myahse <myahse-email@example.com>
2025-03-10 00:18:19 +0000

    heheh

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,8 +1,9 @@
  /** @type {import('tailwindcss').Config} */
+ import daisyui from 'daisyui'
  export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 }
